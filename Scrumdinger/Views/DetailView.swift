9d2a0b88233eed10982f5e1baeae138363bb5ca5
peter-lee97 <peter.leejd@gmail.com>
2024-01-22 17:54:32 +0800

    added navigation stacks to details scrum view

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,45 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Peter Lee on 22/1/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack{
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text("\(scrum.theme.name)")
+                        .padding()
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
+    }
+}
+
+struct DailyView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack{
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -10,13 +10,29 @@ import SwiftUI
 struct ScrumsView: View {
     let scrums: [DailyScrum]
     var body: some View {
-        List(scrums) {
-            scrum in CardView(scrum: scrum)
-                .listRowBackground(scrum.theme.mainColor)
+        NavigationStack{
+            List(scrums) {scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)){
+                    CardView(
+                        scrum: scrum
+                    )
+                }.listRowBackground(
+                    scrum.theme.mainColor
+                )
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar{
+                Button(action: {}){
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
 
 #Preview {
-    ScrumsView(scrums: DailyScrum.sampleData)
+    ScrumsView(
+        scrums: DailyScrum.sampleData
+    )
 }
