c2b582826227cc41b913657c2a61ccd07ffc09e3
peter-lee97 <peter.leejd@gmail.com>
2024-01-23 13:22:57 +0800

    Added navigation stack and navigation link to navigate between screens

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -12,9 +12,12 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink (destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+    
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -25,14 +28,19 @@ struct DetailView: View {
                     Label("Theme", systemImage: "paintpalette")
                     Spacer()
                     Text("\(scrum.theme.name)")
-                        .padding()
+                        .padding(4)
                         .foregroundColor(scrum.theme.accentColor)
                         .background(scrum.theme.mainColor)
                         .cornerRadius(4)
                 }
                 .accessibilityElement(children: .combine)
             }
-        }
+            Section(header: Text("Attendees")) {
+                ForEach (scrum.attendees){attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }.navigationTitle(scrum.title)
     }
 }
 
